222a8138a2a806203edca23a6c07bdc17ef39ad4
Andre Neves <drakferion@gmail.com>
2012-03-20 03:24:55 +0100

    it works <3

--- conway.js
+++ conway.js
@@ -16,11 +16,13 @@ $(document).ready(function() {
   var canvas;
   var ctx;
   
-  var width = 30;
+  var width = 10;
   var cell_width;
-  var height = 30;
+  var height = 10;
   var cell_height;
-  var map;
+
+  var state;
+  var settings;
   
   var methods = {
     init : function() {
@@ -28,17 +30,44 @@ $(document).ready(function() {
 		ctx = canvas.getContext('2d');
 		cell_width = canvas.width/width;
 		cell_height = canvas.height/height;
-		map = new Array(height);
-		for(var i = 0; i < height; i++) {
-			map[i] = new Array(width);
-		}
 		methods.wipe();
 	},
+	wipe : function() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+    },
+	draw : function() {
+		methods.wipe();
+		for(var y = 0; y < height; y++) {
+			for(var x = 0; x < width; x++) {
+				if(state.world[y][x] != 0) {
+					methods.drawPoint(x, y);
+				}
+			}
+		}
+	},
+	drawPoint : function( x, y ) {
+		var x1 = x*cell_width;
+		var y1 = y*cell_height;
+		ctx.fillStyle = "rgb(200,0,0)";
+		ctx.fillRect (x1, y1, cell_width, cell_height);
+	},
+    initWorld : function() {
+		var world = new Array(height);
+		for(var y = 0; y < height; y++) {
+			world[y] = new Array(width);
+			for(var x = 0; x < width; x++) {
+				world[y][x] = 0;
+			}
+		}
+		return world;
+	},
     run : function( options ) {
-		var settings = $.extend( {
+		settings = $.extend( {
 		  'born'		: 3,
-		  'survive-min'	: 2,
-		  'survive-max'	: 2,
+		  'survive'	: {
+			'min'	: 2,
+			'max'	: 3
+		  },
 		  'map'			: [
 			[1, 1],
 			[2, 1],
@@ -47,41 +76,83 @@ $(document).ready(function() {
 			[2, 3],
 		  ]
 		}, options);
-		
-		this.data('conway-map', settings.map);
 
-		methods.draw(settings.map);
+		var wo = methods.initWorld();
+		var x;
+		var y;
+		var born = settings.born;
+		for each(point in settings.map) {
+			x = point[0];
+			y = point[1];
+			wo[y][x] = born;
+		}
+		state = {
+			world : wo,
+			old_world : methods.initWorld()
+		};
+
+		methods.draw();
     },
 	step : function() {
-		var map = this.data('conway-map');
-		var newmap = methods.update(map);
-		this.data('conway-map', newmap);
-		methods.draw(map);
+		methods.update();
+		methods.draw();
 	},
-	update : function(oldMap) {
-		var map = new Array();
-		var i = 0;
-		for each( point in oldMap ) {
-			point[0]++;
-			map[i++] = point;
+	update : function() {
+		var wo = state.old_world; // hand me the leftover memory from yesterday's dinner...
+		var old = state.world;
+		// init
+		for(var y=0; y<width; y++) {
+			for(var x=0; x<height; x++) {
+				wo[y][x] = 0;
+			}
 		}
+		// count neighbours
+		for(var y=0; y<width; y++) {
+			for(var x=0; x<height; x++) {
+				if( old[y][x] > 0 ) {
+					methods.incrementNeighbours(wo, x, y);
+				}
+			}
+		}
+		// cull
+		for(var y=0; y<width; y++) {
+			for(var x=0; x<height; x++) {
+				var neighbours = wo[y][x];
+				if( old[y][x] == 0 ) { // Born?
+					if( neighbours != settings.born ) {
+						wo[y][x] = 0; // Almost; try again.
+					}
+				} else { // Or stay alive?
+					if( neighbours < settings.survive.min
+						|| neighbours > settings.survive.max
+						) {
+						wo[y][x] = 0; // Almost; try again.
+					}
+				}
+			}
+		}
+		state.world = wo;
+		state.old_world = old;
 	},
-	wipe : function() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	incrementNeighbours : function(world, x, y) {
+		if(x > 0) {
+			methods.incrementNeighboursY(world, x-1, y);
+		}
+		methods.incrementNeighboursY(world, x, y);
+		world[y][x]--;
+		if(x < width-1) {
+			methods.incrementNeighboursY(world, x+1, y);
+		}
     },
-	draw : function( map ) {
-		methods.wipe();
-
-		for each( point in map ) {
-			methods.drawPoint(point[0], point[1]);
+	incrementNeighboursY : function(world, x, y) {
+		if(y > 0) {
+			world[y-1][x]++;
 		}
-	},
-	drawPoint : function( x, y ) {
-		var x1 = x*cell_width;
-		var y1 = y*cell_height;
-		ctx.fillStyle = "rgb(200,0,0)";
-		ctx.fillRect (x1, y1, cell_width, cell_height);
-	}
+		world[y][x]++;
+		if(y < height-1) {
+			world[y+1][x]++;
+		}
+    },
   };
 
   $.fn.conway = function( method ) {
