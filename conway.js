02dc611f48d30d86252919d6237c38ccb9b03d11
Andre Neves <drakferion@gmail.com>
2012-04-22 21:06:05 +0200

    Draws a grid.
    Also some more maps.

--- conway.js
+++ conway.js
@@ -51,14 +51,17 @@
 	});
 
 	var maps = {
-		'T':	[[21, 21], [22, 21], [23, 21], [22, 22], [22, 23], [22, 24]],
+		'T':			[[21, 21], [22, 21], [23, 21], [22, 22], [22, 23], [22, 24]],
 		'glider':		[[4, 3], [5, 3], [6, 3], [6, 2], [5, 1]],
 		'LWSS':			[[3, 4], [4, 3], [5, 3], [6, 3], [7, 3], [7, 4], [7, 5], [6, 6], [3, 6]],
 		'pulsar':		[[4, 2], [5, 2], [6, 2], [4, 7], [5, 7], [6, 7], [2, 4], [2, 5], [2, 6], [7, 4], [7, 5], [7, 6],
 						 [9, 4], [9, 5], [9, 6], [14, 4], [14, 5], [14, 6], [10, 2], [11, 2], [12, 2], [10, 7], [11, 7], [12, 7],
 						 [4, 9], [5, 9], [6, 9], [4, 14], [5, 14], [6, 14], [2, 10], [2, 11], [2, 12], [7, 10], [7, 11], [7, 12],
 						 [10, 9], [11, 9], [12, 9], [10, 14], [11, 14], [12, 14], [9, 10], [9, 11], [9, 12], [14, 10], [14, 11], [14, 12]
-						 ]
+						 ],
+		'infinite min':	[[20,19],[18,20],[20,20],[21,20],[18,21],[20,21],[18,22],[16,23],[14,24],[16,24]],
+		'infinite 2':	[[14,18],[15,18],[16,18],[18,18],[14,19],[17,20],[18,20],[15,21],[16,21],[18,21],[14,22],[16,22],[18,22]],
+		'infinite line':[[4,14],[5,14],[6,14],[7,14],[8,14],[9,14],[10,14],[11,14],[13,14],[14,14],[15,14],[16,14],[17,14],[21,14],[22,14],[23,14],[30,14],[31,14],[32,14],[33,14],[34,14],[35,14],[36,14],[38,14],[39,14],[40,14],[41,14],[42,14]]
 		},
 		default_settings = {
 			'born'		: 3,
@@ -91,6 +94,7 @@
 			play: $('.controls button[data-action="play"]'),
 			faster: $('.controls button[data-action="faster"]'),
 			stop: $('.controls button[data-action="stop"]'),
+			drawGrid: $('.controls button[data-action="drawGrid"]'),
 			gene: $('#generation'),
 			speed: $('#speed'),
 			mode: $('#mode'),
@@ -155,24 +159,7 @@
 					mouseup:	finishPainting
 				});
 			},
-			loadMap : function () {
-				var map = JSON.parse($('#map').val());
-				settings.map = map;
-				methods.zero_gen();
-			},
-			exportMap : function () {
-				var x, y, map = [];
-				for (y = 0; y < settings.height; y++) {
-					for (x = 0; x < settings.width; x++) {
-						if (state.world[y][x] !== 0) {
-							map.push([x,y]);
-						}
-					}
-				}
-				ui.output.text("Current map: \n" + JSON.stringify(map));
-			},
 			init : function (options) {
-				methods.wipe();
 				settings = $.extend(default_settings, options);
 				cell_width = canvas.width / settings.width;
 				cell_height = canvas.height / settings.height;
@@ -209,11 +196,28 @@
 					old_world : methods.initWorld()
 				};
 
+				methods.wipe();
 				methods.draw();
 				ui.step.removeAttr('disabled');
 				ui.slower.removeAttr('disabled');
 				ui.play.removeAttr('disabled');
 				ui.faster.removeAttr('disabled');
+			},
+			loadMap : function () {
+				var map = JSON.parse($('#map').val());
+				settings.map = map;
+				methods.zero_gen();
+			},
+			exportMap : function () {
+				var x, y, map = [];
+				for (y = 0; y < settings.height; y++) {
+					for (x = 0; x < settings.width; x++) {
+						if (state.world[y][x] !== 0) {
+							map.push([x,y]);
+						}
+					}
+				}
+				ui.output.text("Current map: \n" + JSON.stringify(map));
 			},
 			wipe : function () {
 				ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -222,6 +226,10 @@
 				var x,
 					y;
 				methods.wipe();
+				ctx.fillStyle = "red";
+				ctx.strokeStyle = "black";
+				ctx.lineWidth = 0.5;
+				methods.drawGrid();
 				for (y = 0; y < settings.height; y++) {
 					for (x = 0; x < settings.width; x++) {
 						if (state.world[y][x] !== 0) {
@@ -231,6 +239,36 @@
 				}
 				ui.gene.text(state.generation);
 			},
+			toggleGrid : function() {
+				state.withGrid = !state.withGrid;
+				methods.draw();
+			},
+			drawGrid : function() {
+				var x,
+					gridStepX = cell_width,
+					y,
+					gridStepY = cell_height;
+				if (!state.withGrid) {
+					return;
+				}
+				while (gridStepX * 32 < canvas.width) {
+					gridStepX *= 2;
+				}
+				while (gridStepY * 32 < canvas.height) {
+					gridStepY *= 2;
+				}
+				ctx.beginPath();
+				for (y = gridStepY; y < canvas.height; y += gridStepY) {
+					ctx.moveTo(0, y);
+					ctx.lineTo(canvas.width, y);
+				}
+				for (x = gridStepX; x < canvas.width; x += gridStepX) {
+					ctx.moveTo(x, 0);
+					ctx.lineTo(x, canvas.height);
+				}
+				ctx.closePath();
+				ctx.stroke();
+			},
 			wipePoint : function (x, y) {
 				var x1 = x * cell_width,
 					y1 = y * cell_height;
@@ -239,7 +277,6 @@
 			drawPoint : function (x, y) {
 				var x1 = x * cell_width,
 					y1 = y * cell_height;
-				ctx.fillStyle = "rgb(200,0,0)";
 				ctx.fillRect(x1, y1, cell_width, cell_height);
 			},
 			initWorld : function () {
